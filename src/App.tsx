76e9c253aaede4daa89cf2501e1aa569d3248c69
Bryan Swift <mail@bryanwrit.es>
2019-12-12 22:02:03 -0500

    Use `state` to decided what to display in `App`

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import React, { useReducer } from 'react';
 import { SubredditSelector } from './components/SubredditSelector';
+import { SubredditView } from './components/SubredditView';
 import { INITIAL_STATE, reducer } from './AppState';
 import './App.css';
 
 const App: React.FC = () => {
   const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
+  const { subreddit } = state;
   console.log(state);
-  return (
-    <div className="App">
-      <SubredditSelector dispatch={dispatch} />
-    </div>
-  );
+  if (subreddit === '') {
+    return (
+      <div className="App">
+        <SubredditSelector dispatch={dispatch} />
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <SubredditView {...state} dispatch={dispatch} />
+      </div>
+    );
+  }
 }
 
 export default App;
