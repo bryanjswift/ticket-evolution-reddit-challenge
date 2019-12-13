8d265577901d89da2f20b220fad1a5f064b52844
Bryan Swift <mail@bryanwrit.es>
2019-12-12 20:18:13 -0500

    Add documentation for `Post` and associated types

--- src/model/Post.ts
+++ src/model/Post.ts
@@ -1,28 +1,60 @@
+/**
+ * Type alias for an id associated with a `Post`.
+ */
+type PostId = string;
+
+/**
+ * Type alias for the id associated with `Image`.
+ */
 type ImageId = string;
 
+/**
+ * Value union for `post_hint` used in the `Post` interface. Returned by the
+ * Reddit API.
+ */
 type PostType = 'image';
 
+/**
+ * Necessary data from the Reddit API to render and filter posts.
+ *
+ * Inferred based on https://www.reddit.com/r/${subreddit}.json where
+ * `subreddit` is `"pics"` or `"programming"`.
+ */
 export interface Post {
-  id: string;
+  /** Identifier for the Reddit post. */
+  id: PostId;
+  /** A hint indicating probable type for the Reddit post. */
   post_hint: PostType;
+  /** Information about previewing the post. */
   preview: PostPreview;
+  /** The number of upvotes the post has received. */
   ups: number;
+  /** Permalink to the post on reddit.com */
   permalink: string;
 }
 
+/**
+ * Data required to show a preview for an 'image' `PostType`.
+ */
 interface PostPreview {
   images: Image[];
 }
 
 /**
- *
+ * API data about a collection of image assets.
  */
 interface Image {
+  /** Identifier for the image. */
   id: ImageId;
+  /** `ImageAsset` representing the "source" or "original" image. */
   source: ImageAsset;
+  /** `ImageAsset` instances representing the `source` at different sizes. */
   resolutions: ImageAsset[];
 }
 
+/**
+ * API data about an image asset which can be displayed.
+ */
 interface ImageAsset {
   url: string;
   width: number;
