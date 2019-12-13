5a3ecc11b029eed8e9c59fc58883294b38380220
Bryan Swift <mail@bryanwrit.es>
2019-12-12 22:19:30 -0500

    Fetch post data when `isLoading` is during `App` render
    This is a gamble. `App` _should_ only render once with `isLoading` true
    but this should be more tightly managed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import React, { useReducer } from 'react';
+import React, { Dispatch, useReducer } from 'react';
 import { SubredditSelector } from './components/SubredditSelector';
 import { SubredditView } from './components/SubredditView';
+import { Action } from './Action';
+import { fetchPosts } from './Api';
 import { INITIAL_STATE, reducer } from './AppState';
 import './App.css';
 
+async function fetchPostData(subreddit: string, dispatch: Dispatch<Action>) {
+  try {
+    const posts = await fetchPosts({ name: subreddit });
+    dispatch({ type: 'ReceivePosts', posts });
+  } catch (e) {
+    dispatch({ type: 'ReceiveErrors', errors: [e.message] });
+  }
+}
+
 const App: React.FC = () => {
   const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
-  const { subreddit } = state;
+  const { isLoading, subreddit } = state;
   console.log(state);
+  if (isLoading) {
+    fetchPostData(subreddit, dispatch);
+  }
   if (subreddit === '') {
     return (
       <div className="App">
