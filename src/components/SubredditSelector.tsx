374832214a1a0eed915093ea248858396c865dc8
Bryan Swift <mail@bryanwrit.es>
2019-12-12 21:24:30 -0500

    Handle form submission logic within `SubredditSelector`

--- src/components/SubredditSelector.tsx
+++ src/components/SubredditSelector.tsx
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { FormEvent, useRef, useState } from 'react';
+import { Action } from '../Action';
+import { fetchPosts } from '../Api';
 
 export function SubredditSelector() {
+  const inputEl = useRef<HTMLInputElement>(null);
+  const [errors, setErrors] = useState<string[]>([]);
+  async function onSelectorSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    if (inputEl.current === null || inputEl.current.value === '') {
+      setErrors(['Subreddit must have a value']);
+    } else {
+      try {
+        const posts = await fetchPosts({ name: inputEl.current.value });
+        console.log(posts);
+      } catch (e) {
+        setErrors([e.message]);
+      }
+    }
+  }
   return (
-    <form>
+    <form action="/" method="POST" onSubmit={onSelectorSubmit}>
+      <Errors messages={errors} />
       <label>
         <span>Subreddit</span>
-        <input type="text" name="subreddit" />
+        <input ref={inputEl} type="text" name="subreddit" />
       </label>
       <button type="submit">Go</button>
     </form>
   );
+}
+
+function Errors(props: { messages: string[] }) {
+  const { messages } = props;
+  if (messages.length > 1) {
+    return (
+      <>
+        {messages.map(m => <span key={m}>{m}</span>)}
+      </>
+    );
+  } else {
+    return null;
+  }
 }
